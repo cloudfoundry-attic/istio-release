74daa4ca705d8c6296ed9f02532691d47ae4b44f
Christian Ang <cang@pivotal.io>
2018-05-08 14:58:42 -0700

    Add iptables REDIRECT rule to test-linux
    [#157072277]
    
    Signed-off-by: Aidan Obley <aobley@pivotal.io>

--- test-linux.sh
+++ test-linux.sh
@@ -6,6 +6,11 @@ cd "$(dirname "$0")"
 export GOPATH=${PWD}
 export PATH=$PATH:$GOPATH/bin
 
+# Setup iptables REDIRECT rule for CloudFoundry Pilot test
+sudo apt update
+sudo apt install -y iptables
+sudo iptables -t nat -A OUTPUT -d 127.1.1.1/32 -p tcp -j REDIRECT --to-port 15001
+
 pushd src/code.cloudfoundry.org/copilot
   dep ensure
   go install ./vendor/github.com/onsi/ginkgo/ginkgo
@@ -20,3 +25,5 @@ pushd src/istio.io/istio
   make test/local/cloudfoundry/e2e_pilotv2
 popd
 
+# Clean up iptables rule
+sudo iptables -t nat -D OUTPUT -d 127.1.1.1/32 -p tcp -j REDIRECT --to-port 15001
