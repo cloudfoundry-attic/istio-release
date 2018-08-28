174d6ce28256fbfa27750b1ffc570927a52e9861
Kauana dos Santos <kdossantos@pivotal.io>
2018-08-28 14:04:21 -0700

    Remove dep ensure

--- test-linux.sh
+++ test-linux.sh
@@ -7,14 +7,12 @@ export GOPATH=${PWD}
 export PATH=$PATH:$GOPATH/bin
 
 pushd src/code.cloudfoundry.org/copilot
-  dep ensure
   go install ./vendor/github.com/onsi/ginkgo/ginkgo
   ginkgo -r -p --randomizeAllSpecs --randomizeSuites --failOnPending --trace --race --progress
 popd
 
 pushd src/istio.io/istio
   export KUBECONFIG=$PWD/.circleci/config
-  dep ensure
   make localTestEnv
   make pilot-test
   make test/local/cloudfoundry/e2e_pilotv2
