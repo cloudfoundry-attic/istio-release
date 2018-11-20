53deb5bf2f99567050d2e4f90c17b7f9aacb3af9
Utako Ueda <uueda@pivotal.io>
2018-11-20 11:17:11 -0800

    Split tests
    run Istio release and Istio upstream tests separately
    they require different docker images now that we're running rspec tests
    
    Co-authored-by: Aaron Hurley <ahurley@pivotal.io>

--- test-upstream-linux.sh
+++ test-upstream-linux.sh
@@ -6,15 +6,6 @@ cd "$(dirname "$0")"
 export GOPATH=${PWD}
 export PATH=$PATH:$GOPATH/bin
 
-bundle
-bundle exec rspec
-
-pushd src/code.cloudfoundry.org/copilot
-  dep ensure
-  go install ./vendor/github.com/onsi/ginkgo/ginkgo
-  ginkgo -r -p --randomizeAllSpecs --randomizeSuites --failOnPending --trace --race --progress
-popd
-
 pushd src/istio.io/istio
   export KUBECONFIG=$PWD/.circleci/config
   make localTestEnv
