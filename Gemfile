2d0b8f2efd64d66491586fd97c5b3ee427eae0ac
Tyler Schultz <tschultz@pivotal.io>
2019-02-21 15:19:33 -0800

    Upgrade bosh-template gem and tests accordingly
    Signed-off-by: Clay Kauzlaric <ckauzlaric@pivotal.io>

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rspec', '~> 3.0'
-gem 'bosh-template', '~> 1.3262', '>= 1.3262.24.0'
+gem 'bosh-template'
 gem 'semi_semantic', '~> 1.2'
 gem 'rubocop', '~> 0.49.0'
 gem 'json'
--- spec/copilot.json.erb_spec.rb
+++ spec/copilot.json.erb_spec.rb
@@ -2,7 +2,7 @@
 # rubocop:disable BlockLength
 require 'rspec'
 require 'yaml'
-require 'bosh/template/evaluation_context'
+require 'bosh/template/test'
 require 'json'
 
 TEST_CERT = 'some
@@ -18,48 +18,44 @@ multi line key'.freeze
 describe 'config.json.erb' do
   let(:deployment_manifest_fragment) do
     {
-      'index' => 0,
-      'job' => { 'name' => 'i_like_bosh' },
-      'properties' => {
-        'listen_port_for_mcp' => 9009,
-        'pilot_client_ca_cert' => 'pilot-ca',
-        'listen_port_for_cloud_controller' => 9001,
-        'listen_port_for_vip_resolver' => 9002,
-        'cloud_controller_ca_cert' => 'cc-ca',
-        'server_cert' => 'server-cert',
-        'server_key' => 'server-key',
-        'experimental_vip_cidr' => '127.128.0.0/9',
-        'log_level' => 'fatal',
-        'bbs' => {
-          'sync_interval' => '30s',
-          'server_ca_cert' => 'bbs-ca',
-          'client_cert' => 'bbs-client',
-          'client_key' => 'bbs-key',
-          'address' => 'https://bbs.service.cf.internal:8889',
-          'client_session_cache_size' => 0,
-          'max_idle_conns_per_host' => 0
+      'listen_port_for_mcp' => 9009,
+      'pilot_client_ca_cert' => 'pilot-ca',
+      'listen_port_for_cloud_controller' => 9001,
+      'listen_port_for_vip_resolver' => 9002,
+      'cloud_controller_ca_cert' => 'cc-ca',
+      'server_cert' => 'server-cert',
+      'server_key' => 'server-key',
+      'experimental_vip_cidr' => '127.128.0.0/9',
+      'log_level' => 'fatal',
+      'bbs' => {
+        'sync_interval' => '30s',
+        'server_ca_cert' => 'bbs-ca',
+        'client_cert' => 'bbs-client',
+        'client_key' => 'bbs-key',
+        'address' => 'https://bbs.service.cf.internal:8889',
+        'client_session_cache_size' => 0,
+        'max_idle_conns_per_host' => 0
+      },
+      'frontend_tls_keypairs' => [
+        {
+          'cert_chain' => 'test-chain',
+          'private_key' => 'test-key'
         },
-        'frontend_tls_keypairs' => [
-          {
-            'cert_chain' => 'test-chain',
-            'private_key' => 'test-key'
-          },
-          {
-            'cert_chain' => 'test-chain2',
-            'private_key' => 'test-key2'
-          }
-        ]
-      }
+        {
+          'cert_chain' => 'test-chain2',
+          'private_key' => 'test-key2'
+        }
+      ]
     }
   end
 
-  let(:erb_yaml) do
-    File.read(File.join(File.dirname(__FILE__), '../jobs/copilot/templates/config.json.erb'))
-  end
+  let(:release_path) { File.join(File.dirname(__FILE__), '..') }
+  let(:release) { Bosh::Template::Test::ReleaseDir.new(release_path) }
+  let(:job) { release.job('copilot') }
 
   subject(:parsed_yaml) do
-    binding = Bosh::Template::EvaluationContext.new(deployment_manifest_fragment).get_binding
-    JSON.parse(ERB.new(erb_yaml).result(binding))
+    template = job.template('config/config.json')
+    JSON.parse(template.render(deployment_manifest_fragment))
   end
 
   context 'given a generally valid manifest' do
@@ -76,7 +72,7 @@ describe 'config.json.erb' do
 
       context 'when an incorrect frontend_tls_keypairs value is provided with missing cert' do
         before do
-          deployment_manifest_fragment['properties']['frontend_tls_keypairs'] = [{ 'private_key' => 'test-key' }]
+          deployment_manifest_fragment['frontend_tls_keypairs'] = [{ 'private_key' => 'test-key' }]
         end
 
         it 'should error' do
@@ -86,7 +82,7 @@ describe 'config.json.erb' do
 
       context 'when an incorrect frontend_tls_keypairs value is provided with missing key' do
         before do
-          deployment_manifest_fragment['properties']['frontend_tls_keypairs'] = [{ 'cert_chain' => 'test-chain' }]
+          deployment_manifest_fragment['frontend_tls_keypairs'] = [{ 'cert_chain' => 'test-chain' }]
         end
 
         it 'should error' do
@@ -96,7 +92,7 @@ describe 'config.json.erb' do
 
       context 'when an incorrect frontend_tls_keypairs value is provided as wrong format' do
         before do
-          deployment_manifest_fragment['properties']['frontend_tls_keypairs'] = ['cert']
+          deployment_manifest_fragment['frontend_tls_keypairs'] = ['cert']
         end
         it 'should error' do
           expect { raise parsed_yaml }.to raise_error(RuntimeError, 'must provide cert_chain and private_key with frontend_tls_keypairs')
@@ -105,7 +101,7 @@ describe 'config.json.erb' do
 
       context 'when an incorrect frontend_tls_keypairs value is not provided as an array' do
         before do
-          deployment_manifest_fragment['properties']['frontend_tls_keypairs'] = 'cert'
+          deployment_manifest_fragment['frontend_tls_keypairs'] = 'cert'
         end
         it 'should error' do
           expect { raise parsed_yaml }.to raise_error(RuntimeError, 'frontend_tls_keypairs should be an array')
